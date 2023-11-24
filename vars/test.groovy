4157ebec0eee87a1e6bb0e9d053d7da45b53f358
docdoom12345 <103731163+docdoom12345@users.noreply.github.com>
2023-11-24 16:03:38 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,10 +1,1 @@
-def call(Closure action) {
-    try {
-        action()
-    } catch(e){
-        emailext    body: 'the execution finished',
-                    subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - ERROR!',
-                    to: 'kidflash76@gmail.com'
-        throw err
-    }   
-}
+
